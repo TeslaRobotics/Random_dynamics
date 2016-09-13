37c9ab5de173b943237a83f0691fd0a85b2db816
Pablito <you@example.com>
2016-09-13 16:47:40 -0500

    pull apart ball tracking and rotor tracking

--- Data Acquisition and motion filtering/videoDataAdq.m
+++ Data Acquisition and motion filtering/videoDataAdq.m
@@ -15,38 +15,19 @@ rotorBlobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
 
 foregroundDetector = vision.ForegroundDetector('NumGaussians', 3,'NumTrainingFrames', 50);
 
-for i = 1:30
-    videoFrame = step(videoReader);
-    step(foregroundDetector,videoFrame);
-end
-reset(videoReader); 
-
 path = 'C:\Documents and Settings\Pablito\My Documents\MATLAB\Roulette\Data Acquisition and motion filtering\rsc\images\';
 ballFilterMask  = imread(strcat(path, 'ball_mask.bmp'));
 rotorFilterMask = imread(strcat(path, 'rotor_mask.bmp'));
 ballTresholdMask = imread(strcat(path, 'tresh_mask.bmp'));
 
 
-ballPositionArray = zeros(1,2);
-rotorPositionArray = zeros(1,2);
 i = 1;
-while  ~isDone(videoReader)
+rotorPositionArray = ones(1,2);
+while ~isDone(videoReader)
+    videoFrame = step(videoReader);
+    ballForegroundCell{i} = step(foregroundDetector,videoFrame);   
     
-    videoFrame = step(videoReader);    
-    foreground = step(foregroundDetector,videoFrame);
-    
-    maskedForeground = and(foreground,ballFilterMask);
-    [area,cent,~] = step(ballBlobAnalysis, maskedForeground);
-    
-     if ~isempty(area)
-         [~,I] = max(area);
-         ballPositionArray(i,:) = cent(I,:);         
-     else
-         ballPositionArray(i,:) = [1,1];
-     end
-
-     
-     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     r = videoFrame(:,:,1);
     g = videoFrame(:,:,2);
@@ -55,22 +36,40 @@ while  ~isDone(videoReader)
     bw = f>0.03;
     bw = and(bw,rotorFilterMask);
     
-    [area2,cent2,~] = step(rotorBlobAnalysis, bw);
+    [area,centroid,~] = step(rotorBlobAnalysis, bw);
     
-    if ~isempty(area2)
-        [~,I2] = max(area2);
-        rotorPositionArray(i,:) = cent2(I2,:);
+    if ~isempty(area)
+        [~,I2] = max(area);
+        rotorPositionArray(i,:) = centroid(I2,:);
     else
         rotorPositionArray(i,:) = [1,1];
     end
     
+    i = i + 1;
+end
+
+
+ballPositionArray = zeros(1,2);
+
+for  j = 1:numel(ballForegroundCell)
+       
+    foreground = ballForegroundCell{j};
+    
+    maskedForeground = and(foreground,ballFilterMask);
+    [area,centroid,~] = step(ballBlobAnalysis, maskedForeground);
+    
+     if ~isempty(area)
+         [~,I] = max(area);
+         ballPositionArray(i,:) = centroid(I,:);         
+     else
+         ballPositionArray(i,:) = [1,1];
+     end
+     
     ballPosition = round(ballPositionArray(i,:));
-    if and(ballTresholdMask(ballPosition(2),ballPosition(1)),i > 100)
+    if and(ballTresholdMask(ballPosition(2),ballPosition(1)),j > 100)
         break;
     end
-    
-    i = i+1;
-end
+ end
 %  release(videoPlayer);
 %  release(videoReader);
 %  delete(videoPlayer); 
